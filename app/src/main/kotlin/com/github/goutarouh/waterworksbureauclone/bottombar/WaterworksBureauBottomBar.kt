478952c464c367f93cc6fa87f86b5b4c12b5a66a
goutarouh <gouta.gorogoro@gmail.com>
2022-11-15 20:00:28 +0900

    #9 タブアイコンを設定

--- app/src/main/kotlin/com/github/goutarouh/waterworksbureauclone/bottombar/WaterworksBureauBottomBar.kt
+++ app/src/main/kotlin/com/github/goutarouh/waterworksbureauclone/bottombar/WaterworksBureauBottomBar.kt
@@ -10,28 +10,58 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import com.github.goutarouh.waterworksbureauclone.R
+
+enum class BottomNavItem(
+    val stringRes: Int,
+    val imageVector: ImageVector,
+    val order: Int
+) {
+    HOME(R.string.home, Icons.Filled.Home,0),
+    NOTIFICATION(R.string.notification, Icons.Filled.Notifications, 1),
+    PAY(R.string.pay, Icons.Filled.Payment, 2),
+    HISTORY(R.string.history, Icons.Filled.History, 3),
+    MENU(R.string.menu, Icons.Filled.MoreHoriz, 4)
+}
 
 @Composable
 fun WaterworksBureauBottomBar() {
-    val list = listOf("1", "2", "3", "4")
+    val items = BottomNavItem.values().sortedBy { it.order }
     val selectedIndex = remember { mutableStateOf(0) }
     WaterWorksBureauBottomNavLayout(
         selectedIndex = selectedIndex.value,
-        itemCount = list.size,
+        itemCount = items.size,
         dropletIndicator = { Droplet() }
     ) {
-        list.forEachIndexed { index, s ->
+        items.forEachIndexed { index, item ->
             val selected = index == selectedIndex.value
             val tint = if (selected) MaterialTheme.colors.primary else Color.White
             WaterWorksBureauBottomNavigationIem(
+                icon = {
+                    Icon(
+                        imageVector = item.imageVector,
+                        tint = tint,
+                        contentDescription = null
+                    )
+                },
                 text = {
-                    Text(text = "item$s", color = tint)
+                    Text(
+                        text = stringResource(item.stringRes),
+                        fontSize = 12.sp,
+                        color = tint
+                    )
                 },
                 selected = selected,
                 onSelected = {
